453e8882b3a4fc16bb64ec4c102267c3f924e3b2
joseteodoro <jteodoro.dev@gmail.com>
2017-09-26 22:47:54 -0300

    adding tests using manual connection

--- stackExchangeQuestions/test/POCConnection.js
+++ stackExchangeQuestions/test/POCConnection.js
@@ -1,18 +1,54 @@
-
-const Stackexchange = require('stackexchange-node');
 const encodeUrl = require('encodeurl');
 
-describe ('POC', () => {
-  it('encode tags', (done) => {
+describe('POC', () => {
+  it('request using manual connection', (done) => {
+    const request = require('request');
+    const query = {sort: 'votes',
+      order: 'desc',
+      site: 'softwareengineering',
+      fromdate: '1320969600',
+      todate: '1506475291',
+      pagesize: '10',
+      page: '1',
+      tagged: 'programming-practices',
+      answered: 'false'
+    };
+
+    const requestParams = {
+      method: 'GET',
+      uri: 'https://api.stackexchange.com/2.2/questions',
+      gzip: 'true',
+      json: 'body',
+      qs: query
+    };
+
+    request(requestParams, (error, response, body) => {
+      if (!error) {
+        console.log(response);
+        if (response.body && response.body.items) {
+          response.body.items.map((item) => {
+            return {score: item.score, link: item.link, title: item.title};
+          }).forEach((scored) => {
+            console.log(scored);
+          });
+        }
+      } else {
+        console.log(`error: ${response}`);
+      }
+      done();
+    });
+  });
+  it.skip('encode tags', (done) => {
     console.log(encodeUrl('good-practices'));
     done();
   });
-  it('connect on SE and get some data', (done) => {
+  it.skip('connect on SE and get some data', (done) => {
+    const Stackexchange = require('stackexchange-node');
     const apiOptions = { version: 2.2 };
     const api = new Stackexchange(apiOptions);
     const filter = {
       pagesize: 5,
-      tagged: ['pattern'],
+      tagged: ['programming-practices'],
       sort: 'votes',
       order: 'desc',
       site: 'softwareengineering'
